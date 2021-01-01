e5896c2588b5040e02471b19720c79fce7a1a5de
Luis Felipe Cerero Garcia <luisfelipe.cerero@kycus.io>
2021-01-01 16:41:41 -0500

    Organizando estructura del proyecto

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
+// Este se encarga de todo lo que se haga en el elemento, a empujar al navegador.
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-// import HolaMundo from './components/HolaMundo';
-import Button from './components/Button';
-import reportWebVitals from './reportWebVitals';
+// Elementos creados  
+import HelloWorld from './components/HelloWorld'
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    {/* <HolaMundo /> */}
-    <Button text="Click Dos"/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <HelloWorld></HelloWorld>,
+    document.getElementById(app)
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
